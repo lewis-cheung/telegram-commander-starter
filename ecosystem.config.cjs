4cf48db1bae24ea015d0d48eaefe85915ebef3bd
Lewis Cheung <lewislunsj@gmail.com>
2025-01-16 15:13:00 -0500

    fix: restructure

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-	apps : [{
-		name: 'telegram-commander-starter',
-		script: 'npm run start',
-		max_memory_restart: '512M',
-		combine_logs: true,
-	}],
+  apps : [{
+    name: 'telegram-commander-starter',
+    script: 'npm run start',
+    max_memory_restart: '512M',
+    combine_logs: true,
+  }],
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import TelegramCommanderApp from './app/index.js'
-import config from './app/config.js'
+import config from './config.js'
 
 new TelegramCommanderApp(config.telegram.token)
