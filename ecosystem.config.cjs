d5291b5d396232532c7fe302d2cd0b658df3d3b9
Lewis Cheung <lewislunsj@gmail.com>
2025-01-16 17:20:19 -0500

    feat: dev app to watch changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,15 @@
 module.exports = {
-  apps : [{
-    name: 'telegram-commander-starter',
-    script: 'npm run start',
-    max_memory_restart: '512M',
-    combine_logs: true,
-  }],
+  apps : [
+    {
+      name: 'telegram-commander-starter',
+      script: 'npm run start',
+      max_memory_restart: '512M',
+    },
+    {
+      name: 'telegram-commander-starter-dev',
+      script: 'npm run start',
+      max_memory_restart: '512M',
+      watch: ['app/**/*.js', 'config.yaml'],
+    }
+  ],
 }
