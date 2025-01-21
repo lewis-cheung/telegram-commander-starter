a34870ed2c600f4d00d36e210eb4bd160c9c5cd6
Lewis Cheung <lewislunsj@gmail.com>
2025-01-20 21:57:35 -0500

    feat: user model functions

--- app/models/user.js
+++ app/models/user.js
@@ -11,14 +11,47 @@ const UserStatus = {
 const userSchema = new mongoose.Schema({
   chatId: { type: Number, required: true },
   status: { type: String, required: true, enum: Object.values(UserStatus) },
+  lastCommandAt: { type: Date, default: Date.now },
 }, {
   timestamps: true,
 })
 
 const User = mongoose.model('User', userSchema)
 
+/**
+ * Create a new user
+ * @param {Number} chatId
+ * @param {UserStatus} status
+ * @returns {Promise<User>}
+ */
+function createUser(chatId, status) {
+  const user = new User({ chatId, status })
+  return user.save()
+}
+
+/**
+ * Get a user by chatId
+ * @param {Number} chatId
+ * @returns {Promise<User>}
+ */
+function getUserByChatId(chatId) {
+  return User.findOne({ chatId })
+}
+
+/**
+ * Get a user by id
+ * @param {string} id
+ * @returns {Promise<User>}
+ */
+function getUserById(id) {
+  return User.findById(id)
+}
+
 export {
   User,
   UserStatus,
   userSchema,
+  createUser,
+  getUserByChatId,
+  getUserById,
 }
