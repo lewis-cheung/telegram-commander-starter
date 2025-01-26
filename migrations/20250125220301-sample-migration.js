fe6ed79cef87d1579d612ff9d04dd87934540d31
Lewis Cheung <lewislunsj@gmail.com>
2025-01-25 23:41:28 -0500

    feat: migration typings

--- migrations/20250125220301-sample-migration.js
+++ migrations/20250125220301-sample-migration.js
@@ -1,7 +1,15 @@
+/**
+ * @param {import('mongodb').Db} db
+ * @param {import('mongodb').MongoClient} client
+ */
 export const up = async (db, client) => {
     await db.collection('sample').insertOne({ name: 'sample' })
 }
 
+/**
+ * @param {import('mongodb').Db} db
+ * @param {import('mongodb').MongoClient} client
+ */
 export const down = async (db, client) => {
   await db.collection('sample').deleteOne({ name: 'sample' })
 }
